caef11fdcad6cfd0ee58f8332c4cb05a59691846
ValerijsDiks <virtuoso292@gmail.com>
2022-03-07 21:03:25 +0200

    Added deeds calculation

--- SuperHero/Program.cs
+++ SuperHero/Program.cs
@@ -40,6 +40,16 @@ namespace SuperHero // Note: actual namespace depends on the project name.
                 - Average time spent on one deed
                 - How many cookies Hero will get. 5 cookies per hour
              */
+
+            int deedTimeInHours1, deedTimeInHours2, deedTimeInHours3;
+            deedTimeInHours1 = 5;
+            deedTimeInHours2 = 10;
+            deedTimeInHours3 = 15;
+            var totalTimeSpent = (deedTimeInHours1 + deedTimeInHours2 + deedTimeInHours3);
+            double rewardMoney = totalTimeSpent * 5;
+            salary += rewardMoney;
+            var averageTime = totalTimeSpent / 3;
+
             double cookieCost = 1.29;
             double boughtCookies = Math.Floor(salary / cookieCost);
             double dailySalary = Math.Round(salary / 30, 2);
@@ -48,6 +58,9 @@ namespace SuperHero // Note: actual namespace depends on the project name.
             Console.WriteLine("************FINANCIAL INFO*****************");
             Console.WriteLine($"The hero can buy {boughtCookies}");
             Console.WriteLine($"Our hero earns daily {dailySalary}");
+            Console.WriteLine($"Our hero spent {totalTimeSpent} hours doing deeds");
+            Console.WriteLine($"Our hero on average spent {averageTime} hours per deed");
+            Console.WriteLine($"For the deeds our hero got as a reward {rewardMoney} EUR");
 
             if (!isEvil)
             {
