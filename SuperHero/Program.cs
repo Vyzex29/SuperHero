d9c37d80ae6b0029d6b7b96666f6d7662944f7fb
ValerijsDiks <virtuoso292@gmail.com>
2022-03-07 20:52:20 +0200

    Add Cookie calculation

--- SuperHero/Program.cs
+++ SuperHero/Program.cs
@@ -20,12 +20,45 @@ namespace SuperHero // Note: actual namespace depends on the project name.
             heroPower1 = "Can compile .net code in his mind";
             heroPower2 = "Knows all of .net versions quirks";
             heroPower3 = "Knows all the answers to your .net questions";
-            Console.WriteLine("********************************************");
+            double salary = 1000; // Monthly
+            bool isEvil = false;
+
+            Console.WriteLine("*********************GENERAL INFO******************");
             Console.WriteLine($"Hero: {name}");
             Console.WriteLine("Age: " + age + " year old");
             Console.WriteLine("Hero powers: \n {0}, \n {1},\n {2}\n", heroPower1, heroPower2, heroPower3);
-            Console.WriteLine($"Hero powers: {heroPower1},  {heroPower2}, {heroPower3}");
-            Console.WriteLine("********************************************");
+            Console.WriteLine("******************************************** \n \n");
+
+
+
+            // Cookie calculation
+            /*
+             * - Continue working on Hero Card view
+                - Add new variables - deedTimeInHours1, deedTimeInHours2, deedTimeInHours3 (INT), holding time that a deed took to complete
+                 - Add methods to calculate:
+                - Total time spent on deeds
+                - Average time spent on one deed
+                - How many cookies Hero will get. 5 cookies per hour
+             */
+            double cookieCost = 1.29;
+            double boughtCookies = Math.Floor(salary / cookieCost);
+            double dailySalary = Math.Round(salary / 30, 2);
+
+
+            Console.WriteLine("************FINANCIAL INFO*****************");
+            Console.WriteLine($"The hero can buy {boughtCookies}");
+            Console.WriteLine($"Our hero earns daily {dailySalary}");
+
+            if (!isEvil)
+            {
+                Console.WriteLine("Protects the city and earns his cookies");
+            }
+            else
+            {
+                Console.WriteLine("The villain is stealing the cookie supply");
+            }
+
+
         }
     }
 }
